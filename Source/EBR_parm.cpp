de18bf3c60d06f1e7d563918b4d51a83f962ccf3
Fr13ndSDP <2583067591@qq.com>
2023-06-27 14:51:49 +0800

    adjust parameters

--- Source/EBR_parm.cpp
+++ Source/EBR_parm.cpp
@@ -5,5 +5,6 @@ void Parm::Initialize ()
     constexpr amrex::Real Ru = amrex::Real(8.31451);
     Rg = Ru/eos_gamma;
     cv = Ru / (eos_mu * (eos_gamma-amrex::Real(1.0)));
-    cp = eos_gamma * Ru / (eos_mu * (eos_gamma-amrex::Real(1.0)));
+    cp = eos_gamma * cv;
+    kOverMu = cp/Pr;
 }
