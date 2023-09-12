65bda673d3bd9f4c4df8c3026c6be6f5fda77d17
Fr13ndSDP <2583067591@qq.com>
2023-09-12 22:27:25 +0800

    add KH test case

---
+++ Exec/8_KH/prob.cpp
@@ -0,0 +1,23 @@
+#include <AMReX_PROB_AMR_F.H>
+#include <AMReX_ParmParse.H>
+#include "prob_parm.H"
+#include "EBR.H"
+#include "IndexDefines.H"
+
+extern "C" {
+    void amrex_probinit (const int* /*init*/,
+                         const int* /*name*/,
+                         const int* /*namelen*/,
+                         const amrex_real* /*problo*/,
+                         const amrex_real* /*probhi*/)
+    {
+        amrex::ParmParse pp("prob");
+
+        // pp.query("p_l", EBR::h_prob_parm->p_l);
+        // pp.query("p_r", EBR::h_prob_parm->p_r);
+        // pp.query("rho_l", EBR::h_prob_parm->rho_l);
+        // pp.query("rho_r", EBR::h_prob_parm->rho_r);
+
+        Gpu::copy(amrex::Gpu::hostToDevice, EBR::h_prob_parm, EBR::h_prob_parm+1, EBR::d_prob_parm);
+    }
+}
